2e2358a4312a6360fe745bff80c86fd49ce456e3
Starmy <mf99250@gmail.com>
2020-11-24 20:57:53 +0800

    Initial Commit

---
+++ Ship/ShipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShipApp.swift
+//  Ship
+//
+//  Created by User19 on 2020/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ShipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
